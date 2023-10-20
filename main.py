015fa5eb7f225465b50312dfd3f01b758b9c16ae
Aditya Kale <kaditya21comp@student.mes.ac.in>
2023-10-19 17:21:04 -0700

    Fix hardcoded stock list

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ from flask import Flask,render_template
 
 app = Flask(__name__)
 
+symbol_list = ["Nifty 50", "Nifty Bank", "Nifty Financial Services","Reliance"]
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html',symbol_list=symbol_list)
 
 
 app.run(host='0.0.0.0', port=81)
