371541f0dbf26f8dca75b32a5a40b20bac8345e9
Aditya Kale <kaditya21comp@student.mes.ac.in>
2023-10-24 20:00:26 +0530

    Fix: Exporting Min-Max

--- src/model_creation.py
+++ src/model_creation.py
@@ -317,38 +317,6 @@ def main():
 
     print(f'\n\x1b[1;32mModels Saved Successfully\x1b[0m\n')
 
-# function to perform Min-Max scaling with bias
-def min_max_scaling_with_bias(column, bias_min=None, bias_max=None):
-    # calculate the minimum and maximum values of the column
-    column_min = column.min()
-    column_max = column.max()
-
-    # add bias to the minimum and maximum values if specified
-    if bias_min is not None:
-        column_min -= bias_min
-    if bias_max is not None:
-        column_max += bias_max
-
-    # perform Min-Max scaling
-    column_scaled = (column - column_min) / (column_max - column_min)
-
-    return column_scaled, column_min, column_max
-
-
-
-# function to perform inverse transformation and return the original values
-def min_max_scaling_inverse(column_scaled, column_min, column_max, bias_min=None, bias_max=None):
-    # remove bias from the minimum and maximum values if specified
-    if bias_min is not None:
-        column_min += bias_min
-    if bias_max is not None:
-        column_max -= bias_max
-
-    # perform inverse transformation
-    column_unscaled = column_scaled * (column_max - column_min) + column_min
-
-    return column_unscaled
-
 
 if __name__ == "__main__":
     main()
