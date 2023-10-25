637ca35f30fd979443e54aa42089ba335019ba6a
Aditya Kale <kaditya21comp@student.mes.ac.in>
2023-10-25 20:47:49 +0530

    Feature: Charts on Selection

--- main.py
+++ main.py
@@ -6,18 +6,20 @@ import json
 app = Flask(__name__)
 
 symbol_list = ["Nifty 50", "Nifty Bank", "Nifty Financial Services","Reliance"]
+dict_list = ["nifty50","niftybank","niftyfin","reliance"]
+n=len(dict_list)
 
 @app.route('/')
 def index():
-    return render_template('index.html',symbol_list=symbol_list)
+    return render_template('index.html',symbol_list=symbol_list,dict_list=dict_list,n=n)
 
 chart_data = [
     {"time": "2023-10-23","value": 19281.75},
     {"time": "2023-10-24","value": 19290}
 ]
 
-@app.route('/chart-data')
-def get_chart_data():    
+@app.route('/chart-data-nifty50')
+def get_nifty50_chart_data():    
     print(f'\x1b[1;36mGetting Dates\x1b[0m\n')
     end_date = datetime.now()
     start_date = end_date - timedelta(500)
@@ -34,8 +36,8 @@ def get_chart_data():
     chart_data = [{"time": date.strftime("%Y-%m-%d"), "value": close} for date, close in zip(df_nifty50["Date"], df_nifty50["Close"])]
     return jsonify(chart_data)
 
-@app.route('/pred')
-def get_pred():
+@app.route('/pred-nifty50')
+def get_nifty50_pred():
     end_date = datetime.now()
     start_date = end_date - timedelta(500)
     start_timestamp = int(round(datetime.timestamp(start_date),0))
@@ -48,11 +50,128 @@ def get_pred():
     df_nifty50.drop(['Open','High','Low','Adj Close','Volume'],axis=1,inplace=True)
     df_nifty50["Date"] = pd.to_datetime(df_nifty50["Date"])
     df_nifty50 = df_nifty50.tail(1)
+    df_nifty50.dropna(inplace=True)
     with open('src/prediction.json', 'r') as json_file:
         data = json.load(json_file)
     new_row = {'Date':end_date+timedelta(1),'Close':data['pred_nifty50']}
     df_nifty50.loc[len(df_nifty50)] = new_row
     chart_data = [{"time": date.strftime("%Y-%m-%d"), "value": close} for date, close in zip(df_nifty50["Date"], df_nifty50["Close"])]
+    return jsonify(chart_data)
+
+
+
+@app.route('/chart-data-niftybank')
+def get_niftybank_chart_data():    
+    print(f'\x1b[1;36mGetting Dates\x1b[0m\n')
+    end_date = datetime.now()
+    start_date = end_date - timedelta(500)
+    start_timestamp = int(round(datetime.timestamp(start_date),0))
+    end_timestamp = int(round(datetime.timestamp(end_date),0))
+    print(f'\x1b[1;36mStart Date        \x1b[1;32m{start_date.strftime("%Y-%b-%d")}\x1b[0m')
+    print(f'\x1b[1;36mEnd Date          \x1b[1;32m{end_date.strftime("%Y-%b-%d")}\x1b[0m\n')
+
+    nifty_bank_url = f'https://query1.finance.yahoo.com/v7/finance/download/%5ENSEBANK?period1={start_timestamp}&period2={end_timestamp}&interval=1d&events=history&includeAdjustedClose=true'
+    df_niftybank = pd.read_csv(nifty_bank_url)
+    df_niftybank.drop(['Open','High','Low','Adj Close','Volume'],axis=1,inplace=True)
+    df_niftybank["Date"] = pd.to_datetime(df_niftybank["Date"])
+    df_niftybank.dropna(inplace=True)
+    chart_data = [{"time": date.strftime("%Y-%m-%d"), "value": close} for date, close in zip(df_niftybank["Date"], df_niftybank["Close"])]
+    return jsonify(chart_data)
+
+@app.route('/pred-niftybank')
+def get_niftybank_pred():
+    end_date = datetime.now()
+    start_date = end_date - timedelta(500)
+    start_timestamp = int(round(datetime.timestamp(start_date),0))
+    end_timestamp = int(round(datetime.timestamp(end_date),0))
+    print(f'\x1b[1;36mStart Date        \x1b[1;32m{start_date.strftime("%Y-%b-%d")}\x1b[0m')
+    print(f'\x1b[1;36mEnd Date          \x1b[1;32m{end_date.strftime("%Y-%b-%d")}\x1b[0m\n')
+    nifty_bank_url = f'https://query1.finance.yahoo.com/v7/finance/download/%5ENSEBANK?period1={start_timestamp}&period2={end_timestamp}&interval=1d&events=history&includeAdjustedClose=true'
+    df_niftybank = pd.read_csv(nifty_bank_url)
+    df_niftybank.drop(['Open','High','Low','Adj Close','Volume'],axis=1,inplace=True)
+    df_niftybank["Date"] = pd.to_datetime(df_niftybank["Date"])
+    df_niftybank = df_niftybank.tail(1)
+    with open('src/prediction.json', 'r') as json_file:
+        data = json.load(json_file)
+    new_row = {'Date':end_date+timedelta(1),'Close':data['pred_niftybank']}
+    df_niftybank.loc[len(df_niftybank)] = new_row
+    chart_data = [{"time": date.strftime("%Y-%m-%d"), "value": close} for date, close in zip(df_niftybank["Date"], df_niftybank["Close"])]
+    return jsonify(chart_data)
+
+@app.route('/chart-data-niftyfin')
+def get_niftyfin_chart_data():    
+    print(f'\x1b[1;36mGetting Dates\x1b[0m\n')
+    end_date = datetime.now()
+    start_date = end_date - timedelta(500)
+    start_timestamp = int(round(datetime.timestamp(start_date),0))
+    end_timestamp = int(round(datetime.timestamp(end_date),0))
+    print(f'\x1b[1;36mStart Date        \x1b[1;32m{start_date.strftime("%Y-%b-%d")}\x1b[0m')
+    print(f'\x1b[1;36mEnd Date          \x1b[1;32m{end_date.strftime("%Y-%b-%d")}\x1b[0m\n')
+
+    nifty_fin_services_url = f'https://query1.finance.yahoo.com/v7/finance/download/NIFTY_FIN_SERVICE.NS?period1={start_timestamp}&period2={end_timestamp}&interval=1d&events=history&includeAdjustedClose=true'
+    df_niftyfin = pd.read_csv(nifty_fin_services_url)
+    df_niftyfin.drop(['Open','High','Low','Adj Close','Volume'],axis=1,inplace=True)
+    df_niftyfin["Date"] = pd.to_datetime(df_niftyfin["Date"])
+    df_niftyfin.dropna(inplace=True)
+    chart_data = [{"time": date.strftime("%Y-%m-%d"), "value": close} for date, close in zip(df_niftyfin["Date"], df_niftyfin["Close"])]
+    return jsonify(chart_data)
+
+@app.route('/pred-niftyfin')
+def get_niftyfin_pred():
+    end_date = datetime.now()
+    start_date = end_date - timedelta(500)
+    start_timestamp = int(round(datetime.timestamp(start_date),0))
+    end_timestamp = int(round(datetime.timestamp(end_date),0))
+    print(f'\x1b[1;36mStart Date        \x1b[1;32m{start_date.strftime("%Y-%b-%d")}\x1b[0m')
+    print(f'\x1b[1;36mEnd Date          \x1b[1;32m{end_date.strftime("%Y-%b-%d")}\x1b[0m\n')
+    nifty_fin_services_url = f'https://query1.finance.yahoo.com/v7/finance/download/NIFTY_FIN_SERVICE.NS?period1={start_timestamp}&period2={end_timestamp}&interval=1d&events=history&includeAdjustedClose=true'
+    df_niftyfin = pd.read_csv(nifty_fin_services_url)
+    df_niftyfin.drop(['Open','High','Low','Adj Close','Volume'],axis=1,inplace=True)
+    df_niftyfin["Date"] = pd.to_datetime(df_niftyfin["Date"])
+    df_niftyfin = df_niftyfin.tail(1)
+    with open('src/prediction.json', 'r') as json_file:
+        data = json.load(json_file)
+    new_row = {'Date':end_date+timedelta(1),'Close':data['pred_niftyfin']}
+    df_niftyfin.loc[len(df_niftyfin)] = new_row
+    chart_data = [{"time": date.strftime("%Y-%m-%d"), "value": close} for date, close in zip(df_niftyfin["Date"], df_niftyfin["Close"])]
+    return jsonify(chart_data)
+
+@app.route('/chart-data-reliance')
+def get_reliance_chart_data():    
+    print(f'\x1b[1;36mGetting Dates\x1b[0m\n')
+    end_date = datetime.now()
+    start_date = end_date - timedelta(500)
+    start_timestamp = int(round(datetime.timestamp(start_date),0))
+    end_timestamp = int(round(datetime.timestamp(end_date),0))
+    print(f'\x1b[1;36mStart Date        \x1b[1;32m{start_date.strftime("%Y-%b-%d")}\x1b[0m')
+    print(f'\x1b[1;36mEnd Date          \x1b[1;32m{end_date.strftime("%Y-%b-%d")}\x1b[0m\n')
+
+    reliance_url = f'https://query1.finance.yahoo.com/v7/finance/download/RELIANCE.NS?period1={start_timestamp}&period2={end_timestamp}&interval=1d&events=history&includeAdjustedClose=true'
+    df_reliance = pd.read_csv(reliance_url)
+    df_reliance.drop(['Open','High','Low','Adj Close','Volume'],axis=1,inplace=True)
+    df_reliance["Date"] = pd.to_datetime(df_reliance["Date"])
+    df_reliance.dropna(inplace=True)
+    chart_data = [{"time": date.strftime("%Y-%m-%d"), "value": close} for date, close in zip(df_reliance["Date"], df_reliance["Close"])]
+    return jsonify(chart_data)
+
+@app.route('/pred-reliance')
+def get_reliance_pred():
+    end_date = datetime.now()
+    start_date = end_date - timedelta(500)
+    start_timestamp = int(round(datetime.timestamp(start_date),0))
+    end_timestamp = int(round(datetime.timestamp(end_date),0))
+    print(f'\x1b[1;36mStart Date        \x1b[1;32m{start_date.strftime("%Y-%b-%d")}\x1b[0m')
+    print(f'\x1b[1;36mEnd Date          \x1b[1;32m{end_date.strftime("%Y-%b-%d")}\x1b[0m\n')
+    reliance_url = f'https://query1.finance.yahoo.com/v7/finance/download/RELIANCE.NS?period1={start_timestamp}&period2={end_timestamp}&interval=1d&events=history&includeAdjustedClose=true'
+    df_reliance = pd.read_csv(reliance_url)
+    df_reliance.drop(['Open','High','Low','Adj Close','Volume'],axis=1,inplace=True)
+    df_reliance["Date"] = pd.to_datetime(df_reliance["Date"])
+    df_reliance = df_reliance.tail(1)
+    with open('src/prediction.json', 'r') as json_file:
+        data = json.load(json_file)
+    new_row = {'Date':end_date+timedelta(1),'Close':data['pred_reliance']}
+    df_reliance.loc[len(df_reliance)] = new_row
+    chart_data = [{"time": date.strftime("%Y-%m-%d"), "value": close} for date, close in zip(df_reliance["Date"], df_reliance["Close"])]
     return jsonify(chart_data)
 
 
--- static/js/chartScript.js
+++ static/js/chartScript.js
@@ -1,30 +1,55 @@
 document.addEventListener("DOMContentLoaded", function () {
     // Create a container for the chart
-    const chartContainer = document.getElementById("chart-container");
 
-    // Create a new chart
-    const chart = LightweightCharts.createChart(chartContainer, {
-        width: chartContainer.clientWidth,
-        height: chartContainer.clientHeight,
+    function fetchData(symbol) {
+
+        const chartContainer = document.getElementById("chart-container");
+
+        // Create a new chart
+        const chart = LightweightCharts.createChart(chartContainer, {
+            width: chartContainer.clientWidth,
+            height: chartContainer.clientHeight,
+        });
+
+        // Create line series for the data and prediction
+        const lineSeries = chart.addLineSeries({ color: 'blue' });
+        const predSeries = chart.addLineSeries({ color: 'red' });
+
+        // Fetch data for the "data" series
+        fetch(`/chart-data-${symbol}`)
+            .then(response => response.json())
+            .then(data => {
+                // Set the data for the "data" series
+                lineSeries.setData(data);
+            });
+
+        // Fetch data for the "prediction" series
+        fetch(`/pred-${symbol}`)
+            .then(response => response.json())
+            .then(data => {
+                // Set the data for the "prediction" series
+                predSeries.setData(data);
+            });
+    }
+
+    fetchData('nifty50')
+
+    function updateData(symbol) {
+        // Clear existing chart container and legend
+        const chartContainer = document.getElementById('chart-container');
+        chartContainer.innerHTML = '';
+
+        fetchData(symbol);
+    }
+
+    const symbolRows = document.getElementsByClassName('symbol-row');
+    console.log(symbolRows);
+    Array.from(symbolRows).forEach(row => {
+        row.addEventListener('click', function () {
+            selectedSymbol = this.dataset.symbol;
+            console.log(selectedSymbol)
+            updateData(selectedSymbol)
+        });
     });
 
-    // Create line series for the data and prediction
-    const lineSeries = chart.addLineSeries({ color: 'blue' });
-    const predSeries = chart.addLineSeries({ color: 'red' });
-
-    // Fetch data for the "data" series
-    fetch('/chart-data')
-        .then(response => response.json())
-        .then(data => {
-            // Set the data for the "data" series
-            lineSeries.setData(data);
-        });
-
-    // Fetch data for the "prediction" series
-    fetch('/pred')
-        .then(response => response.json())
-        .then(data => {
-            // Set the data for the "prediction" series
-            predSeries.setData(data);
-        });
 });
